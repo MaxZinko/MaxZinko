2ad0c59aca9b1c32736e2039dbd105462a2947ff
MaxZinko <brizing1998@gmail.com>
2017-10-11 11:57:10 +0300

    new angular+page directory changes

---
+++ angular-project/src/app/home/block5/block5.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Block5Component } from './block5.component';
+
+describe('Block5Component', () => {
+  let component: Block5Component;
+  let fixture: ComponentFixture<Block5Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Block5Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Block5Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
